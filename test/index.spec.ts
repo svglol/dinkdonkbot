349986422b3747e123afd7e03a0c1e44315410fd
Matthew Trotman <savageguy@gmail.com>
2024-04-17 16:14:43 +1200

    test: update tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,42 +1,36 @@
-import { createExecutionContext, env, waitOnExecutionContext } from 'cloudflare:test'
+import { SELF, env } from 'cloudflare:test'
 import { describe, expect, it } from 'vitest'
-import worker from '../src/server'
-
-const IncomingRequest = Request<unknown, IncomingRequestCfProperties>
+import '../src/server'
 
 describe('worker fetch router', () => {
   it('responds with discord application id', async () => {
-    const request = new IncomingRequest('http://example.com')
-    const ctx = createExecutionContext()
-    const response = await worker.fetch(request, env, ctx)
-    await waitOnExecutionContext(ctx)
+    const response = await SELF.fetch('http://example.com')
     expect(await response.text()).toBe(`👋 ${env.DISCORD_APPLICATION_ID}`)
   })
 
   it('discord interaction endpoint responds with Bad request signature.', async () => {
-    const request = new IncomingRequest('http://example.com', { method: 'POST' })
-    const ctx = createExecutionContext()
-    const response = await worker.fetch(request, env, ctx)
-    await waitOnExecutionContext(ctx)
+    const response = await SELF.fetch('http://example.com', { method: 'POST' })
     expect(response.status).toBe(401)
     expect(await response.text()).toBe('Bad request signature.')
   })
 
-  // it('twitch eventsub endpoint responds with Signature verification failed', async () => {
-  //   const request = new IncomingRequest('http://example.com/twitch-eventsub', { method: 'POST' })
-  //   const ctx = createExecutionContext()
-  //   const response = await worker.fetch(request, env, ctx)
-  //   await waitOnExecutionContext(ctx)
-  //   expect(response.status).toBe(403)
-  //   expect(await response.text()).toBe('Signature verification failed')
-  // })
+  it('twitch eventsub endpoint responds with Signature verification failed', async () => {
+    const response = await SELF.fetch('http://example.com/twitch-eventsub', { method: 'POST' })
+    expect(response.status).toBe(403)
+    expect(await response.text()).toBe('Signature verification failed')
+  })
 
   it('responds with not found and proper status for /404', async () => {
-    const request = new IncomingRequest('http://example.com/404')
-    const ctx = createExecutionContext()
-    const response = await worker.fetch(request, env, ctx)
-    await waitOnExecutionContext(ctx)
+    const response = await SELF.fetch('http://example.com/404')
     expect(response.status).toBe(404)
     expect(await response.text()).toBe('Not Found.')
+  })
+
+  it('dispatches scheduled event', async () => {
+    const result = await SELF.scheduled({
+      scheduledTime: new Date(1000),
+      cron: '0 0 * * *',
+    })
+    expect(result.outcome).toBe('ok')
   })
 })
--- vitest.config.js
+++ vitest.config.js
@@ -4,9 +4,18 @@ export default defineWorkersConfig({
   test: {
     poolOptions: {
       workers: {
+        singleWorker: true,
         wrangler: { configPath: './wrangler.toml' },
         miniflare: {
-
+          compatibilityDate: '2024-01-01',
+          compatibilityFlags: [
+            'nodejs_compat',
+            'service_binding_extra_handlers',
+          ],
+          bindings: {
+            DISCORD_APPLICATION_ID: '1234567890',
+            TWITCH_EVENT_SECRET: '1234567890',
+          },
         },
       },
     },
