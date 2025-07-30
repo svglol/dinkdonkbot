6b9aea52f3cc0b0f98f375a1c6ddda657bb2e150
Matthew Trotman <savageguy@gmail.com>
2025-07-31 10:40:23 +1200

    feat: update slash commands permissions

--- src/discord/commands.ts
+++ src/discord/commands.ts
@@ -3,6 +3,57 @@
  * and registration.
  */
 
+const PERMISSIONS = {
+  // Basic permissions
+  CREATE_INSTANT_INVITE: '1',
+  KICK_MEMBERS: '2',
+  BAN_MEMBERS: '4',
+  ADMINISTRATOR: '8',
+  MANAGE_CHANNELS: '16',
+  MANAGE_GUILD: '32',
+
+  // Message permissions
+  ADD_REACTIONS: '64',
+  VIEW_AUDIT_LOG: '128',
+  PRIORITY_SPEAKER: '256',
+  STREAM: '512',
+  VIEW_CHANNEL: '1024',
+  SEND_MESSAGES: '2048',
+  SEND_TTS_MESSAGES: '4096',
+  MANAGE_MESSAGES: '8192',
+  EMBED_LINKS: '16384',
+  ATTACH_FILES: '32768',
+  READ_MESSAGE_HISTORY: '65536',
+  MENTION_EVERYONE: '131072',
+  USE_EXTERNAL_EMOJIS: '262144',
+  VIEW_GUILD_INSIGHTS: '524288',
+
+  // Voice permissions
+  CONNECT: '1048576',
+  SPEAK: '2097152',
+  MUTE_MEMBERS: '4194304',
+  DEAFEN_MEMBERS: '8388608',
+  MOVE_MEMBERS: '16777216',
+  USE_VAD: '33554432',
+
+  // Advanced permissions
+  CHANGE_NICKNAME: '67108864',
+  MANAGE_NICKNAMES: '134217728',
+  MANAGE_ROLES: '268435456',
+  MANAGE_WEBHOOKS: '536870912',
+  MANAGE_EMOJIS_AND_STICKERS: '1073741824',
+  USE_APPLICATION_COMMANDS: '2147483648',
+  REQUEST_TO_SPEAK: '4294967296',
+  MANAGE_EVENTS: '8589934592',
+  MANAGE_THREADS: '17179869184',
+  CREATE_PUBLIC_THREADS: '34359738368',
+  CREATE_PRIVATE_THREADS: '68719476736',
+  USE_EXTERNAL_STICKERS: '137438953472',
+  SEND_MESSAGES_IN_THREADS: '274877906944',
+  USE_EMBEDDED_ACTIVITIES: '549755813888',
+  MODERATE_MEMBERS: '1099511627776',
+}
+
 export const INVITE_COMMAND = {
   name: 'invite',
   description: 'Get an invite link to add the bot to your server',
@@ -16,7 +67,7 @@ export const DINKDONK_COMMAND = {
 export const TWITCH_COMMAND = {
   name: 'twitch',
   description: 'Twitch stream notifications',
-  default_member_permissions: '0',
+  default_member_permissions: PERMISSIONS.ADMINISTRATOR,
   dm_permission: false,
   options: [{
     type: 1,
@@ -128,7 +179,7 @@ export const TWITCH_COMMAND = {
 export const EMOTE_COMMAND = {
   name: 'emote',
   description: 'Manage discord custom emotes',
-  default_member_permissions: '0',
+  default_member_permissions: PERMISSIONS.MANAGE_EMOJIS_AND_STICKERS,
   dm_permission: false,
   options: [
     {
@@ -149,14 +200,14 @@ export const EMOTE_COMMAND = {
 export const STEAL_EMOTE_COMMAND = {
   name: 'Steal Emote',
   type: 3,
-  default_member_permissions: '0',
+  default_member_permissions: PERMISSIONS.MANAGE_EMOJIS_AND_STICKERS,
   dm_permission: false,
 }
 
 export const TWITCH_CLIPS_COMMAND = {
   name: 'clips',
   description: 'Manage Twitch clip subscriptions for streamers to be posted hourly',
-  default_member_permissions: '0',
+  default_member_permissions: PERMISSIONS.ADMINISTRATOR,
   dm_permission: false,
   options: [{
     type: 1,
@@ -219,7 +270,7 @@ export const TWITCH_CLIPS_COMMAND = {
 export const KICK_COMMAND = {
   name: 'kick',
   description: 'Kick Stream Notifications',
-  default_member_permissions: '0',
+  default_member_permissions: PERMISSIONS.ADMINISTRATOR,
   dm_permission: false,
   options: [
     {
