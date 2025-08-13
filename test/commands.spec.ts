fc47ee5bff72cb822352b8f00d5aa792fc9917e0
Matthew Trotman <savageguy@gmail.com>
2025-08-13 23:12:56 +1200

    test: add tests for discord slash commands

---
+++ test/commands.spec.ts
@@ -0,0 +1,70 @@
+import { describe, expect, it } from 'vitest'
+import { COMMAND_DEFINITIONS, findHandlerByName } from '../src/discord/commands'
+
+describe('commands all have handlers defined', () => {
+  it('dinkdonk', async () => {
+    expect(findHandlerByName('dinkdonk')).toBeDefined()
+  })
+
+  it('invite', async () => {
+    expect(findHandlerByName('invite')).toBeDefined()
+  })
+
+  it('help', async () => {
+    expect(findHandlerByName('help')).toBeDefined()
+  })
+
+  it('twitch', async () => {
+    expect(findHandlerByName('twitch')).toBeDefined()
+  })
+
+  it('emote', async () => {
+    expect(findHandlerByName('emote')).toBeDefined()
+  })
+
+  it('kick', async () => {
+    expect(findHandlerByName('kick')).toBeDefined()
+  })
+
+  it('twitchClips', async () => {
+    expect(findHandlerByName('clips')).toBeDefined()
+  })
+
+  it('stealEmote', async () => {
+    expect(findHandlerByName('steal emote')).toBeDefined()
+  })
+})
+
+describe('commands all have definitions', () => {
+  it('dinkdonk', async () => {
+    expect(COMMAND_DEFINITIONS.find(c => c.name === 'dinkdonk')).toBeDefined()
+  })
+
+  it('invite', async () => {
+    expect(COMMAND_DEFINITIONS.find(c => c.name === 'invite')).toBeDefined()
+  })
+
+  it('help', async () => {
+    expect(COMMAND_DEFINITIONS.find(c => c.name === 'help')).toBeDefined()
+  })
+
+  it('twitch', async () => {
+    expect(COMMAND_DEFINITIONS.find(c => c.name === 'twitch')).toBeDefined()
+  })
+
+  it('emote', async () => {
+    expect(COMMAND_DEFINITIONS.find(c => c.name === 'emote')).toBeDefined()
+  })
+
+  it('kick', async () => {
+    expect(COMMAND_DEFINITIONS.find(c => c.name === 'kick')).toBeDefined()
+  })
+
+  it('twitchClips', async () => {
+    expect(COMMAND_DEFINITIONS.find(c => c.name === 'clips')).toBeDefined()
+  })
+
+  it('stealEmote', async () => {
+    expect(COMMAND_DEFINITIONS.find(c => c.name === 'Steal Emote')).toBeDefined()
+  })
+})
