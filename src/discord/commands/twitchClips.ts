e4b48396ce23a5ccf3e3f62bd9cd52fceb21c37d
Matthew Trotman <savageguy@gmail.com>
2025-08-29 18:12:27 +1200

    fix: removing needing mention everyone permission for twitch clips

--- src/discord/commands/twitchClips.ts
+++ src/discord/commands/twitchClips.ts
@@ -217,7 +217,7 @@ async function handleTwitchClipsCommand(interaction: APIApplicationCommandIntera
       const channel = edit.options.find(option => option.name === 'discord-channel')
       if (channel) {
         const channelValue = String('value' in channel ? channel.value : '')
-        const permissions = await calculateChannelPermissions(interaction.guild_id!, channelValue, env.DISCORD_APPLICATION_ID, env.DISCORD_TOKEN, [PermissionFlagsBits.SendMessages, PermissionFlagsBits.ViewChannel, PermissionFlagsBits.MentionEveryone])
+        const permissions = await calculateChannelPermissions(interaction.guild_id!, channelValue, env.DISCORD_APPLICATION_ID, env.DISCORD_TOKEN, [PermissionFlagsBits.SendMessages, PermissionFlagsBits.ViewChannel])
         const missingPermissions = Object.entries(permissions.checks)
           .filter(([_, hasPermission]) => !hasPermission)
           .map(([permissionName]) => permissionName)
