3a7b5abcdfdff89c84a0dbd263604623d13775d7
Matthew Trotman <savageguy@gmail.com>
2025-08-29 17:28:31 +1200

    tests: fix failing streams tests

--- test/commands/streams.spec.ts
+++ test/commands/streams.spec.ts
@@ -2,24 +2,24 @@ import { describe, expect, it } from 'vitest'
 import { COMMAND_DEFINITIONS } from '../../src/discord/commands'
 import { findAutoCompleteHandlerByName, findHandlerByName, findMessageComponentHandlerByName, findModalSubmitHandlerByName } from '../../src/util/commandsHelper'
 
-describe('stream', () => {
+describe('streams', () => {
   it('should have command defined', () => {
-    expect(COMMAND_DEFINITIONS.find(c => c.name === 'twitch')).toBeDefined()
+    expect(COMMAND_DEFINITIONS.find(c => c.name === 'streams')).toBeDefined()
   })
 
   it('should have handler defined', () => {
-    expect(findHandlerByName('stream')).toBeDefined()
+    expect(findHandlerByName('streams')).toBeDefined()
   })
 
   it('should not have a modal submit handler defined', () => {
-    expect(findModalSubmitHandlerByName('stream')).toBeUndefined()
+    expect(findModalSubmitHandlerByName('streams')).toBeUndefined()
   })
 
   it('should not have a message component handler defined', () => {
-    expect(findMessageComponentHandlerByName('stream')).toBeUndefined()
+    expect(findMessageComponentHandlerByName('streams')).toBeUndefined()
   })
 
   it('should not have a autocomplete handler defined', () => {
-    expect(findAutoCompleteHandlerByName('stream')).toBeDefined()
+    expect(findAutoCompleteHandlerByName('streams')).toBeDefined()
   })
 })
