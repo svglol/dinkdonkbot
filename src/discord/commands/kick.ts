02e9303040dcd2122d4b1441244018ca40e95ddd
Matthew Trotman <savageguy@gmail.com>
2025-08-29 17:21:43 +1200

    feat: simplify old commands

--- src/discord/commands/kick.ts
+++ src/discord/commands/kick.ts
@@ -1,97 +1,18 @@
 import type { APIApplicationCommandInteraction } from 'discord-api-types/v10'
-import { isChatInputApplicationCommandInteraction } from 'discord-api-types/utils'
-import { ApplicationCommandOptionType } from 'discord-api-types/v10'
-import { buildErrorEmbed, findBotCommandMarkdown, updateInteraction } from '../discord'
+import { PermissionFlagsBits } from 'discord-api-types/v10'
+import { findBotCommandMarkdown, updateInteraction } from '../discord'
 import { interactionEphemeralLoading } from '../interactionHandler'
 
 const KICK_COMMAND = {
   name: 'kick',
   description: 'Kick command has moved to /stream kick',
   type: 1,
-  options: [
-    {
-      name: 'add',
-      description: 'Kick add command has moved to /stream kick add',
-      type: 1,
-    },
-    {
-      name: 'test',
-      description: 'Kick test command has moved to /stream kick test',
-      type: 1,
-    },
-    {
-      name: 'remove',
-      description: 'Kick remove command has moved to /stream kick remove',
-      type: 1,
-    },
-    {
-      name: 'edit',
-      description: 'Kick edit command has moved to /stream kick edit',
-      type: 1,
-    },
-    {
-      name: 'list',
-      description: 'Kick list command has moved to /stream kick list',
-      type: 1,
-    },
-    {
-      name: 'details',
-      description: 'Kick details command has moved to /stream kick details',
-      type: 1,
-    },
-    {
-      name: 'help',
-      description: 'Kick help command has moved to /stream help',
-      type: 1,
-    },
-  ],
+  default_member_permissions: PermissionFlagsBits.Administrator.toString(),
+  dm_permission: false,
 }
 
 async function handleCommand(interaction: APIApplicationCommandInteraction, env: Env, ctx: ExecutionContext) {
-  if (!interaction.data || !isChatInputApplicationCommandInteraction(interaction)) {
-    ctx.waitUntil(updateInteraction(interaction, env, { embeds: [buildErrorEmbed('Invalid interaction', env)] }))
-    return interactionEphemeralLoading()
-  }
-  const option = interaction.data.options?.[0]
-  if (!option) {
-    ctx.waitUntil(updateInteraction(interaction, env, { embeds: [buildErrorEmbed('Invalid interaction', env)] }))
-    return interactionEphemeralLoading()
-  }
-  if (option.type === ApplicationCommandOptionType.Subcommand) {
-    switch (option.name) {
-      case 'add':{
-        ctx.waitUntil(updateInteraction(interaction, env, { content: `${await findBotCommandMarkdown(env, 'kick', 'add')} command has moved to ${await findBotCommandMarkdown(env, 'stream', 'kick', 'add')}` }))
-        break
-      }
-      case 'remove':{
-        ctx.waitUntil(updateInteraction(interaction, env, { content: `${await findBotCommandMarkdown(env, 'kick', 'remove')} command has moved to ${await findBotCommandMarkdown(env, 'stream', 'kick', 'remove')}` }))
-        break
-      }
-      case 'edit':{
-        ctx.waitUntil(updateInteraction(interaction, env, { content: `${await findBotCommandMarkdown(env, 'kick', 'edit')} command has moved to ${await findBotCommandMarkdown(env, 'stream', 'kick', 'edit')}` }))
-        break
-      }
-      case 'details':{
-        ctx.waitUntil(updateInteraction(interaction, env, { content: `${await findBotCommandMarkdown(env, 'kick', 'details')} command has moved to ${await findBotCommandMarkdown(env, 'stream', 'kick', 'details')}` }))
-        break
-      }
-      case 'test':{
-        ctx.waitUntil(updateInteraction(interaction, env, { content: `${await findBotCommandMarkdown(env, 'kick', 'test')} command has moved to ${await findBotCommandMarkdown(env, 'stream', 'kick', 'test')}` }))
-        break
-      }
-      case 'list':{
-        ctx.waitUntil(updateInteraction(interaction, env, { content: `${await findBotCommandMarkdown(env, 'kick', 'list')} command has moved to ${await findBotCommandMarkdown(env, 'stream', 'kick', 'list')}` }))
-        break
-      }
-      case 'help':{
-        ctx.waitUntil(updateInteraction(interaction, env, { content: `${await findBotCommandMarkdown(env, 'kick', 'help')} command has moved to ${await findBotCommandMarkdown(env, 'stream', 'help')}` }))
-        break
-      }
-      default:
-        ctx.waitUntil(updateInteraction(interaction, env, { content: `${await findBotCommandMarkdown(env, 'kick')} command has moved to ${await findBotCommandMarkdown(env, 'stream', 'kick')}` }))
-        break
-    }
-  }
+  ctx.waitUntil(updateInteraction(interaction, env, { content: `${await findBotCommandMarkdown(env, 'kick')} commands have moved to ${await findBotCommandMarkdown(env, 'stream', 'kick')}` }))
   return interactionEphemeralLoading()
 }
 
--- src/discord/commands/multistream.ts
+++ src/discord/commands/multistream.ts
@@ -1,78 +1,17 @@
 import type { APIApplicationCommandInteraction } from 'discord-api-types/v10'
-import { isChatInputApplicationCommandInteraction } from 'discord-api-types/utils'
-import { ApplicationCommandOptionType } from 'discord-api-types/v10'
-import { buildErrorEmbed, findBotCommandMarkdown, updateInteraction } from '../discord'
+import { PermissionFlagsBits } from 'discord-api-types/v10'
+import { findBotCommandMarkdown, updateInteraction } from '../discord'
 import { interactionEphemeralLoading } from '../interactionHandler'
 
 const MULTISTREAM_COMMAND = {
   name: 'multistream',
   description: 'Multistream command has moved to /stream multistream',
-  options: [
-    {
-      name: 'link',
-      description: 'Multistream link command has moved to /stream multistream link',
-      type: 1,
-    },
-    {
-      name: 'edit',
-      description: 'Multistream edit command has moved to /stream multistream edit',
-      type: 1,
-    },
-    {
-      name: 'help',
-      description: 'Multistream help command has moved to /stream help',
-      type: 1,
-    },
-    {
-      name: 'unlink',
-      description: 'Multistream unlink command has moved to /stream multistream unlink',
-      type: 1,
-    },
-    {
-      name: 'list',
-      description: 'Multistream list command has moved to /stream multistream list',
-      type: 1,
-    },
-  ],
+  default_member_permissions: PermissionFlagsBits.Administrator.toString(),
+  dm_permission: false,
 }
 
 async function handleCommand(interaction: APIApplicationCommandInteraction, env: Env, ctx: ExecutionContext) {
-  if (!interaction.data || !isChatInputApplicationCommandInteraction(interaction)) {
-    ctx.waitUntil(updateInteraction(interaction, env, { embeds: [buildErrorEmbed('Invalid interaction', env)] }))
-    return interactionEphemeralLoading()
-  }
-  const option = interaction.data.options?.[0]
-  if (!option) {
-    ctx.waitUntil(updateInteraction(interaction, env, { embeds: [buildErrorEmbed('Invalid interaction', env)] }))
-    return interactionEphemeralLoading()
-  }
-  if (option.type === ApplicationCommandOptionType.Subcommand) {
-    switch (option.name) {
-      case 'link':{
-        ctx.waitUntil(updateInteraction(interaction, env, { content: `${await findBotCommandMarkdown(env, 'multistream', 'link')} command has moved to ${await findBotCommandMarkdown(env, 'stream', 'multistream', 'link')}` }))
-        break
-      }
-      case 'unlink':{
-        ctx.waitUntil(updateInteraction(interaction, env, { content: `${await findBotCommandMarkdown(env, 'multistream', 'unlink')} command has moved to ${await findBotCommandMarkdown(env, 'stream', 'multistream', 'unlink')}` }))
-        break
-      }
-      case 'edit':{
-        ctx.waitUntil(updateInteraction(interaction, env, { content: `${await findBotCommandMarkdown(env, 'multistream', 'edit')} command has moved to ${await findBotCommandMarkdown(env, 'stream', 'multistream', 'edit')}` }))
-        break
-      }
-      case 'list':{
-        ctx.waitUntil(updateInteraction(interaction, env, { content: `${await findBotCommandMarkdown(env, 'multistream', 'list')} command has moved to ${await findBotCommandMarkdown(env, 'stream', 'multistream', 'list')}` }))
-        break
-      }
-      case 'help':{
-        ctx.waitUntil(updateInteraction(interaction, env, { content: `${await findBotCommandMarkdown(env, 'multistream', 'help')} command has moved to ${await findBotCommandMarkdown(env, 'stream', 'help')}` }))
-        break
-      }
-      default:
-        ctx.waitUntil(updateInteraction(interaction, env, { content: `${await findBotCommandMarkdown(env, 'multistream')} command has moved to ${await findBotCommandMarkdown(env, 'stream', 'multistream')}` }))
-        break
-    }
-  }
+  ctx.waitUntil(updateInteraction(interaction, env, { content: `${await findBotCommandMarkdown(env, 'multistream')} commands have moved to ${await findBotCommandMarkdown(env, 'stream', 'multistream')}` }))
   return interactionEphemeralLoading()
 }
 
