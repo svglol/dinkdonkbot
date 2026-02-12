5a32b2f9f16d26f1b1e44f638b2f745a2eb700f3
Matthew Trotman <savageguy@gmail.com>
2026-02-12 16:26:51 +1300

    feat: update pageHero configuration for improved background styling

--- docs/app/app.config.ts
+++ docs/app/app.config.ts
@@ -8,7 +8,8 @@ export default defineAppConfig({
     },
     pageHero: {
       slots: {
-        root: 'bg-radial from-yellow-500/10 via-[15%] to-[50%] via-yellow-700/10 to-transparent',
+        // root: 'bg-radial from-yellow-500/10 via-[15%] to-[50%] via-yellow-500/5 to-transparent blur',
+        root: 'relative overflow-hidden before:absolute before:inset-0 before:-z-10 before:bg-radial before:from-yellow-500/10 before:via-[15%] before:via-yellow-500/5 before:to-[70%] before:to-transparent before:blur',
       },
     },
   },
