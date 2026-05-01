ad9a6bfeab10d4fcc2bfd1bcea6dcc9b8cbfdaa3
Matthew Trotman <savageguy@gmail.com>
2026-05-01 13:52:43 +1200

    fix(docs): header icon not working

--- docs/app/components/AppHeaderLogo.vue
+++ docs/app/components/AppHeaderLogo.vue
@@ -1,13 +1,10 @@
 <template>
-  <UColorModeImage
-    :light="appConfig.header?.logo?.light || appConfig.header?.logo?.dark"
-    :dark="appConfig.header?.logo?.dark || appConfig.header?.logo?.light"
-    :alt="appConfig.header?.logo?.alt || appConfig.header?.title"
-    class="h-8 w-auto shrink-0"
-  />
+  <img class="h-8 w-auto shrink-0" :src="usePublicAsset(appConfig.header?.logo?.light || appConfig.header?.logo?.dark)" :alt="appConfig.header?.logo?.alt || appConfig.header?.title">
   {{ appConfig.header?.title || appConfig.header.title }}
 </template>
 
 <script setup lang="ts">
+import { usePublicAsset } from '../composables/usePublicAsset'
+
 const appConfig = useAppConfig()
 </script>
