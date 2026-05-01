227a53bd436acfa903ed0fe0f225a6267d0ea140
Matthew Trotman <savageguy@gmail.com>
2026-05-01 14:02:49 +1200

    fix(docs): sometimes header having wrong font/layout

--- docs/app/components/AppHeaderLogo.vue
+++ docs/app/components/AppHeaderLogo.vue
@@ -1,6 +1,13 @@
 <template>
-  <img class="h-8 w-auto shrink-0" :src="usePublicAsset(appConfig.header?.logo?.light || appConfig.header?.logo?.dark)" :alt="appConfig.header?.logo?.alt || appConfig.header?.title">
-  {{ appConfig.header?.title || appConfig.header.title }}
+  <div class="flex shrink-0 items-end gap-1.5">
+    <img class="h-8 w-auto shrink-0" :src="usePublicAsset(appConfig.header?.logo?.light || appConfig.header?.logo?.dark)" :alt="appConfig.header?.logo?.alt || appConfig.header?.title">
+    <span
+      class="
+        text-xl font-bold text-highlighted
+        focus-visible:outline-primary
+      "
+    >{{ appConfig.header?.title || appConfig.header.title }} </span>
+  </div>
 </template>
 
 <script setup lang="ts">
