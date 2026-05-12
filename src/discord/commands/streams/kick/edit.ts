5c2ec4147f1d5777b9f994796069e4095b2bd371
Matthew Trotman <savageguy@gmail.com>
2026-05-13 11:00:40 +1200

    fix: remove console.error for permission errors in kick edit command

--- src/discord/commands/streams/kick/edit.ts
+++ src/discord/commands/streams/kick/edit.ts
@@ -73,7 +73,6 @@ export async function handleKickEditCommand(interaction: APIApplicationCommandIn
 
       if (missingPermissions.length > 0) {
         const permissionError = `Dinkdonk Bot does not have the required permissions use <#${channel}>.\nMissing permissions: ${missingPermissions.join(', ')}`
-        console.error(permissionError)
         return await updateInteraction(interaction, env, { embeds: [buildErrorEmbed(permissionError, env)] })
       }
       if (dbStream.multiStream) {
